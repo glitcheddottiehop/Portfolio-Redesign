0ec45d29d7d33b6a37327816135a1ae5ba87052a
glitcheddottiehop <smadzenites@gmail.com>
2023-10-13 13:16:09 +0300

    WIP

---
+++ src/src/components/navigation/Navigation.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <div class="navbar-wrapper">
+        <div class="logo">
+            
+        </div>
+        <nav class="navbar">
+            <RouterLink class="navbar-item" to="/">Home</RouterLink>
+            <RouterLink class="navbar-item" to="/about">About</RouterLink>
+            <RouterLink class="navbar-item" to="/projects">Projects</RouterLink>
+            <RouterLink class="navbar-item" to="/contact">Contact me</RouterLink>
+        </nav>
+    </div>
+</template>
+
+<style scoped>
+@import "./navigation.css";
+</style>
