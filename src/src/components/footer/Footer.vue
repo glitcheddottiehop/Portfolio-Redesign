bb728a3030901bbaeb6f5ae8e1c46df9affcb680
glitcheddottiehop <smadzenites@gmail.com>
2023-11-23 19:21:25 +0200

    Add footer

---
+++ src/src/components/footer/Footer.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <footer class="footer-wrapper">
+        <div class="icons">
+            <a href="https://www.linkedin.com/in/santa-vitkovska-5b10251a7/" target="_blank">
+                <img src="../../assets/linkedin.svg" alt="">
+            </a>
+            <img src="../../assets/instagram.svg" alt="">
+            <img src="../../assets/email.svg" alt="">
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+@import "./footer.css";
+</style>
