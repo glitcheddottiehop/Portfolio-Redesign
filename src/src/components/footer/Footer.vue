2aff520769eb67be2c7638fe13ce1ddc1d2ab1bb
glitcheddottiehop <smadzenites@gmail.com>
2023-12-18 14:25:10 +0200

    Add footer links

--- src/src/components/footer/Footer.vue
+++ src/src/components/footer/Footer.vue
@@ -5,11 +5,18 @@ import { RouterLink } from 'vue-router'
 <template>
     <footer class="footer-wrapper">
         <div class="icons">
-            <a href="https://www.linkedin.com/in/santa-vitkovska-5b10251a7/" target="_blank">
+            <a class="link" href="https://www.linkedin.com/in/santa-vitkovska-5b10251a7/" target="_blank">
                 <img src="../../assets/linkedin.svg" alt="">
+                <div class="background background--hover"></div>
             </a>
-            <img src="../../assets/instagram.svg" alt="">
-            <img src="../../assets/email.svg" alt="">
+            <a class="link" href="https://www.instagram.com/glitcheddottiehop.art/" target="_blank">
+                <img src="../../assets/instagram.svg" alt="">
+                <div class="background background--hover"></div>
+            </a>
+            <a class="link" href="mailto:vitkovska98@outlook.com">
+                <img src="../../assets/email.svg" alt="">
+                <div class="background background--hover"></div>
+            </a>
         </div>
     </footer>
 </template>
