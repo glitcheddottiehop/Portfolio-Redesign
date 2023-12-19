1744543ec15584e7e3c8bafefaa1c7c6a6aa6e0c
glitcheddottiehop <smadzenites@gmail.com>
2023-12-19 09:10:38 +0200

    Add landing page

--- src/src/views/HomeView.vue
+++ src/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="title">
+      Aesthetics
+    </div>
+    <div class="quote">
+      Your idea, my skills, our design.
+    </div>
   </main>
 </template>
+
+<style scoped>
+.title {
+  font-size: 200px;
+  font-family: var(--font-family-title);
+  color: var(--color-main-yellow);
+  padding-top: 80px;
+  padding-left: 150px;
+  padding-bottom: 60px;
+}
+.quote {
+  font-size: 28px;
+  text-align: center;
+  font-style: italic;
+}
+</style>
