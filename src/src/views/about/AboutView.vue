bd943e9ad1260c0ead01b9f3fd11427fc252214d
glitcheddottiehop <smadzenites@gmail.com>
2023-12-20 17:27:28 +0200

    Add function to get age

--- src/src/views/about/AboutView.vue
+++ src/src/views/about/AboutView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, type Ref, } from "vue";
+import { getAge } from "./about";
 
 const userCardFlipped: Ref<boolean> = ref(false);
 
@@ -34,16 +35,20 @@ const userCardFlipped: Ref<boolean> = ref(false);
     <div class="card spacing__auto--mlr bio bio-back spacing__lg--plr" @click="userCardFlipped = !userCardFlipped">
       <div class="card__title">More about me...</div>
       <div class="card__subtitle about-me">
-        Hi! My name is Santa. I'm 25 years old. I'm a UI designer who is big friends with Figma. I finished online
-        UI course on 8th of December 2022.
-        Besides that I do have some hobbies too - like drawing, playing harmonica and make something from clay. I
-        make digital art using Adobe Photoshop and post my work on
-        <a href="https://www.instagram.com/glitcheddottiehop.art/" target="_blank" data-bs-toggle="tooltip"
-          title="glitcheddottiehop.art" class="text-primary instagram-link">Instagram</a>.
-        I have also tried Adobe Illustrator and Corel Draw.
-        I finished college as a programming technician and worked in this field for 3 years.
-        Later I realised that I want to work in field closer to my hobby which is art. That's how I decided to
-        start learning about UI design.
+        <div>Hi! My name is Santa. I'm
+          <span id="age">{{ getAge() }}</span>
+          years old.
+          <div>I'm a UI designer who is big friends with <b>Figma</b>. I finished online
+            <b>UX/UI</b> course and now I'm actively participating in various start-up projects.</div>
+          I do have some hobbies too - like drawing and I make digital art post my work on
+          <span class="special-link">
+            <a href="https://www.instagram.com/glitcheddottiehop.art/" target="_blank" data-bs-toggle="tooltip"
+            title="glitcheddottiehop.art" class="text-primary instagram-link">Instagram</a>
+          </span>. (Wow! :O)
+          <div>
+            I finished college as a programming technician and worked in this field for 3 years. You might say I know something from <b>HTML</b> and <b>CSS</b> ;)
+          </div>
+        </div>
       </div>
       <div class="card__wrapper">
         <button class="card__btn card__btn-solid">Back</button>
